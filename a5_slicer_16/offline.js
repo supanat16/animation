7315f14045e1027008e369ddcaecf950ffa42912
supanat16 <s41285@bj.ac.th>
2026-02-12 14:48:36 +0700

    Add files via upload

---
+++ a5_slicer_16/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1770882472,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/fruitslash-sheet0.png",
+		"images/strawberry-sheet0.png",
+		"images/spawner-sheet0.png",
+		"images/botcut-sheet0.png",
+		"images/topcut-sheet0.png",
+		"images/grape_w-sheet0.png",
+		"images/grape_bot-sheet0.png",
+		"images/grape_top-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
