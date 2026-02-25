78e9ff8a44cd64fbc501c10d9754abda9b626623
supanat16 <s41285@bj.ac.th>
2026-02-25 13:45:41 +0700

    Add files via upload

---
+++ a_Final_16_18/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1772001870,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/background.png",
+		"images/sprite-sheet0.png",
+		"images/monter-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/player-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
